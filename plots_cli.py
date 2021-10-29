0d9f6d96be4a77d98afd6bcfad4d73dcb9d696f4
umara <ua9404@rit.edu>
2021-10-29 14:00:55 +0400

    quit

---
+++ plots_cli.py
@@ -0,0 +1,9 @@
+def quit(y, n):
+    input("are you sure (y/n): ")
+    if input == y:
+        print(True)
+    elif input == n:
+        print(False)
+
+quit(y="true", n="false")
+print("Goodbye!")
