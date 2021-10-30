b1e47d5a0998cf72ab3d67d140f26e5f0e690e7a
umara <ua9404@rit.edu>
2021-10-30 14:12:44 +0400

    command

--- plotes.py
+++ plotes.py
@@ -1,12 +1,15 @@
 # This is a sample Python script.
-import plots_cli
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def quit(y, n):
-    input("are you sure: ")
-    assert(y == True)
-    print("goodbye")
-    assert (n == False)
-    print(False)
+    input("are you sure (y/n): ")
+    if input == y:
+        print("Goodbye!")
+    else:
+     if input == n:
+        print("Welcome!")
+
+quit(y="quit", n="continue")
--- plots_cli.py
+++ plots_cli.py
@@ -1,3 +1,6 @@
+
+
+
 def quit(y, n):
     input("are you sure (y/n): ")
     if input == y:
@@ -7,3 +10,18 @@ def quit(y, n):
 
 quit(y="true", n="false")
 print("Goodbye!")
+
+
+input("Enter a command or 'quit' to quit: ")
+try:
+    if input == 'quit':
+     quit(y="goodbye", n="Continue")
+     print("Goodbye!")
+    else:
+        print("Goodbye!")
+except:
+    print("Invalid command")
+    print("Please try again")
+
+
+
